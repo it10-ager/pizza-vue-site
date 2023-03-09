752b593f9d1a6e191d26632af1a29f63d23e935a
German Trushkov <germantrushkov@gmail.com>
2023-03-09 17:56:00 +0200

    repair to work

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: '/pizza-vue-site/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 })
